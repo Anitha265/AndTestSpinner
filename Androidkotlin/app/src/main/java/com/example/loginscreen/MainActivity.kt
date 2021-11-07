4e2964ec7fcc73b9fd366fae6346221cbc8de418
Anitha2605 <satyaanitha2605@gmail.com>
2021-11-07 16:45:25 +0530

    initial files add

---
+++ Androidkotlin/app/src/main/java/com/example/loginscreen/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.loginscreen
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var et_email = findViewById<EditText>(R.id.et_email)
+        var et_pswd = findViewById<EditText>(R.id.et_pswd)
+        var btn_submit = findViewById<Button>(R.id.btn_submit)
+      //  var emailToText = findViewById<Button>(R.id.btn_submit)
+
+        btn_submit.setOnClickListener {
+            val intent = Intent(applicationContext, MainActivity2::class.java )
+            startActivity(intent)
+
+         /*  if(et_email.length() == 0) {
+            Toast.makeText(this, "Email Required",Toast.LENGTH_SHORT).show()
+             return@setOnClickListener
+            }
+            if(et_pswd.length() == 0) {
+                Toast.makeText(this , "Pswd Required",Toast.LENGTH_SHORT).show()
+             return@setOnClickListener
+            }*/
+          /* if (!et_email.toString().trim().isEmpty() && Patterns.EMAIL_ADDRESS.matcher(et_email.toString().trim()).matches()) {
+                Toast.makeText(this, "Email Verified !", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Enter valid Email address !", Toast.LENGTH_SHORT).show();
+            }*/
+
+        }
+
+    }
+}
---
+++ Androidkotlin/app/src/main/java/com/example/loginscreen/MainActivity2.kt
@@ -0,0 +1,42 @@
+package com.example.loginscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+class MainActivity2 : AppCompatActivity(),AdapterView.OnItemSelectedListener {
+
+    var colors = arrayOf("Red" , "Green" , "Blue" , "Orange")
+    lateinit var textView_msg:TextView
+    lateinit var spinner_sample:Spinner
+    lateinit var redCheckBox:CheckBox
+    lateinit var greenCheckBox:CheckBox
+    lateinit var blueCheckBox:CheckBox
+    lateinit var orangeCheckBox:CheckBox
+
+      override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+         textView_msg = findViewById<TextView>(R.id.text_view)
+         spinner_sample = findViewById<Spinner>(R.id.spinner)
+//        redCheckBox = findViewById<CheckBox>(R.id.redCheckBox)
+//        greenCheckBox = findViewById<CheckBox>(R.id.greenCheckBox)
+//        blueCheckBox = findViewById<CheckBox>(R.id.blueCheckBox)
+//        orangeCheckBox = findViewById<CheckBox>(R.id.orangeCheckBox)
+        spinner_sample.adapter = ArrayAdapter(this,R.layout.test_me,colors)
+
+        spinner_sample.setOnItemSelectedListener(this)
+
+        }
+
+    override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
+        textView_msg.text = "Selected : "+colors[position]
+    }
+
+    override fun onNothingSelected(arg0: AdapterView<*>) {
+
+    }
+
+}
